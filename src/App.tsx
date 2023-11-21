4a9b9ec74fa9e545eb5cd28bb4463d48dabd96f4
[SW7] 김지훈 <lanslot84@gmail.com>
2023-11-21 16:03:59 +0900

    FIX : fix header styles

--- src/App.tsx
+++ src/App.tsx
@@ -17,25 +17,25 @@ function App() {
   return (      
     <main style={{ maxWidth: 400, height: "100vh", margin: "0 auto",backgroundColor: "#1e1e1e",position:"relative", }}>
       
-      <button style={{borderRadius: 30, backgroundColor: "#CFFF48", top: "calc(100% - 80px)", left: "calc(100% - 80px)", display: "flex", position: "absolute",
+      <button style={{borderRadius: 30, backgroundColor: "#CFFF48", top: "calc(100% - 70px)", left: "calc(100% - 70px)", display: "flex", position: "absolute",
         width: 48, height: 48, placeItems: "center",placeContent: "center", cursor:"pointer", border:"none"}}>
         <FiPlus color="#1E1E1E" size={24}/>
       </button>
-      <section style={{overflow:"hidden auto", height: "100%", padding: "0 0 5em", }}>
-        <header style={{display: "flex", position: "sticky", top:0, backgroundColor: "#1E1E1E", alignItems: "center", padding: 8}}>
-          <div style={{padding: 12, cursor:"pointer"}}>
+      <section style={{overflow:"hidden auto", height: "100%", padding: "0 0 4em", }}>
+        <header style={{display: "flex", position: "sticky", top:0, backgroundColor: "#1E1E1E", alignItems: "center", padding: 8, boxShadow: "0 1px 0 0 rgba(255,255,255,0.08)"}}>
+          <div style={{padding: 12, cursor:"pointer", display:"flex"}}>
             <FiChevronLeft color="#FFF" size={24}/>
           </div>
           <div style={{flexGrow:1, textAlign:"center", alignItems: "center",display: "flex", flexDirection: "column", justifyContent: "center"}}>
             <div style={{fontSize: 20,fontWeight: 700,}}>{format(currentDate, "MM월 dd일")}</div>
             <div style={{fontSize: 14}}>{format(currentDate, "yyyy년")}</div>
           </div>
-          <div style={{padding: 12, cursor:"pointer"}}>
+          <div style={{padding: 12, cursor:"pointer", display:"flex"}}>
             <FiChevronRight color="#FFF" size={24}/>
           </div>
         </header>
         <div className="todoItemList" 
-            style={{display:"flex", flexDirection: "column",padding: "0 1.25em"}}>
+            style={{display:"flex", flexDirection: "column",padding: "1em 1.25em"}}>
 
           {[...todoItems,...todoItems,...todoItems].map( (todo) =>  {
             return (
