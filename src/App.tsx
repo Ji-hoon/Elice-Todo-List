5d3dad59bb5bfec9186fe1dc9105f31a209ac714
[SW7] 김지훈 <lanslot84@gmail.com>
2023-11-21 16:24:10 +0900

    FIX : 스크롤 이벤트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 // import './App.css';
 
-import { FiCheck, FiPlus, FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { FiCheck, FiPlus, FiChevronLeft, FiChevronRight, FiArrowUpCircle } from "react-icons/fi";
 import todoItemsDummy from './assets/dummy/todoItems.ts';
 import format from 'date-fns/format';
 
@@ -11,8 +11,16 @@ import format from 'date-fns/format';
 function App() {
   const [todoItems, setTodoItems] = useState(todoItemsDummy);
   const [currentDate, setCurrentDate] = useState( new Date() );
-
+  
+  const elementRef = useRef(null);
   console.log(currentDate);
+
+  const scrollToTop = ():void => {
+    const element = elementRef.current;
+    if (element) {
+      element.scrollTo({top:0, behavior:"smooth"});
+    }
+  };
 
   return (      
     <main style={{ maxWidth: 400, height: "100vh", margin: "0 auto",backgroundColor: "#1e1e1e",position:"relative", }}>
@@ -21,7 +29,8 @@ function App() {
         width: 48, height: 48, placeItems: "center",placeContent: "center", cursor:"pointer", border:"none"}}>
         <FiPlus color="#1E1E1E" size={24}/>
       </button>
-      <section style={{overflow:"hidden auto", height: "100%", padding: "0 0 4em", }}>
+      <section ref={elementRef} 
+               style={{overflow:"hidden auto", height: "100%", padding: "0 0 2em", }}>
         <header style={{display: "flex", position: "sticky", top:0, backgroundColor: "#1E1E1E", alignItems: "center", padding: 8, boxShadow: "0 1px 0 0 rgba(255,255,255,0.08)"}}>
           <div style={{padding: 12, cursor:"pointer", display:"flex"}}>
             <FiChevronLeft color="#FFF" size={24}/>
@@ -35,11 +44,11 @@ function App() {
           </div>
         </header>
         <div className="todoItemList" 
-            style={{display:"flex", flexDirection: "column",padding: "1em 1.25em"}}>
+            style={{display:"flex", flexDirection: "column", alignItems:"center",padding: "1em 1.5em"}}>
 
           {[...todoItems,...todoItems,...todoItems].map( (todo) =>  {
             return (
-              <div key={todo.id} index={todo.id} style={{padding: "8px 0"}}>
+              <div key={todo.id} style={{padding: "8px 0", width:"100%"}}>
                 
                 {!todo.isDone &&
                   <div style={{display:'flex', flexDirection: "row", justifyContent:"space-between"}}>
@@ -56,7 +65,10 @@ function App() {
               )
             })
           }
-
+          <div style={{display:"flex", justifyContent:"center", width:"fit-content", margin: "2em 0 0", cursor:"pointer"}}
+              onClick={scrollToTop}>
+            <FiArrowUpCircle color="#505050" size="36"/>
+          </div>
         </div>
       </section>
     </main>
