7460ec4d3892f259dbc2c6b54ee0976ee198d486
[SW7] 김지훈 <lanslot84@gmail.com>
2023-11-23 11:18:59 +0900

    FEAT : add input

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,39 @@ import viteLogo from '/vite.svg';
 import { FiCheck, FiPlus, FiChevronLeft, FiChevronRight, FiArrowUpCircle } from "react-icons/fi";
 import todoItemsDummy from './assets/dummy/todoItems.ts';
 import format from 'date-fns/format';
+import Spacing from "./components/Spacing.tsx";
+import { InputMode, TodoItem } from "./types";
 
+const defaultInputMode: InputMode = {type: "default"};
 
 function App() {
-  const [todoItems, setTodoItems] = useState(todoItemsDummy);
+  const [todoItems, setTodoItems] = useState<TodoItem[]>(todoItemsDummy as TodoItem[]);
   const [currentDate, setCurrentDate] = useState( new Date() );
+
+  const [inputMode, setInputMode] = useState<InputMode>(defaultInputMode);
+  const [addInputValue, setAddInputValue] = useState("");
   
   const elementRef = useRef(null);
   console.log(currentDate);
+
+  function handleAddMode() {
+    setInputMode({type: "add"});
+  }
+
+  function handleResetInputMode() {
+    setInputMode(defaultInputMode);
+  }
+
+  function handleChangeAddInputValue(event:React.ChangeEvent<HTMLInputElement>) {
+    setAddInputValue(event.target.value);
+  }
+
+  function handleAddTodoItem() {
+    // todoItems에 추가하고 인풋모드를 리셋한다.
+    const newTodo = [...todoItems];
+    
+    handleResetInputMode();
+  }
 
   const scrollToTop = ():void => {
     const element = elementRef.current;
@@ -21,11 +46,17 @@ function App() {
       element.scrollTo({top:0, behavior:"smooth"});
     }
   };
-
+  const toggleDone = (id) => {
+    const newTodo = [...todoItems];
+    const targetTodo = newTodo.filter( (todo) => todo.id === id);
+    targetTodo.isDone = !targetTodo.isDone;
+    setTodoItems(newTodo);
+  }
   return (      
     <main style={{ maxWidth: 420, height: "100vh", margin: "0 auto",backgroundColor: "#1e1e1e",position:"relative", }}>
       
-      <button style={{borderRadius: 30, backgroundColor: "#CFFF48", top: "calc(100% - 70px)", left: "calc(100% - 70px)", display: "flex", position: "absolute",
+      <button onClick={handleAddMode}
+      style={{borderRadius: 30, backgroundColor: "#CFFF48", top: "calc(100% - 70px)", left: "calc(100% - 70px)", display: "flex", position: "absolute",
         width: 48, height: 48, placeItems: "center",placeContent: "center", cursor:"pointer", border:"none"}}>
         <FiPlus color="#1E1E1E" size={24}/>
       </button>
@@ -43,17 +74,38 @@ function App() {
             <FiChevronRight color="#FFF" size={24}/>
           </div>
         </header>
+       
         <div className="todoItemList" 
             style={{display:"flex", flexDirection: "column", alignItems:"center",padding: "1em 1em 1em 1.5em"}}>
+          
+          {inputMode.type === "add" && (
+          <div style={{padding:"0",display:"flex",flexDirection: "column", width:"100%"}} >
+            <input placeholder="할 일을 입력하세요"
+                   type="text"
+                  onChange={(e) => handleChangeAddInputValue(e)}
+                  style={{fontSize: "1.25em", padding: "8px 0", 
+                    background: "transparent",
+                    border: "none", 
+                    borderBottom: "2px solid #FFF",
+                    outline: "none", color: "#FFF"}}
+            />
+            <Spacing size={8}/>
+            <div style={{display:"flex", gap: 8}}>
+              <button onClick={handleResetInputMode} style={{border: "2px solid #CFFF48",background:"transparent", color: "#CFFF48", borderRadius: 30, fontWeight: 700, fontSize: "1.05em", padding: "5px 12px 4px"}}>취소</button>
+              <button style={{border:"none", background:"#CFFF48", color: "#000", borderRadius: 30, fontWeight: 700, fontSize: "1.05em", padding: "5px 12px 4px"}}>저장</button>
+            </div>
+          </div>
+          )}
 
-          {[...todoItems,...todoItems,...todoItems].map( (todo) =>  {
+          {[...todoItems].map( (todo) =>  {
             return (
               <div key={todo.id} style={{padding: "0", width:"100%"}}>
                 
                 {!todo.isDone &&
                   <div style={{display:'flex', flexDirection: "row", justifyContent:"space-between"}}>
                     <div style={{display: "flex", alignItems: "center", whiteSpace: "nowrap",overflow: "hidden", textOverflow: "ellipsis"}}>{todo.content}</div>
-                    <div style={{padding: 12, cursor:"pointer", display:"flex"}}>
+                    <div style={{padding: 12, cursor:"pointer", display:"flex"}}
+                        onClick={()=> toggleDone(todo.id)}>
                       <FiCheck size={26}/>
                     </div>
                   </div>}
@@ -62,7 +114,8 @@ function App() {
                   <div style={{display:'flex', flexDirection: "row", justifyContent:"space-between"}}>
                     <div style={{display: "flex", alignItems: "center", whiteSpace: "nowrap",overflow: "hidden", textOverflow: "ellipsis", textDecoration: "line-through", color: "#999"}}>{todo.content}</div>
                     
-                    <div style={{padding: 12, cursor:"pointer", display:"flex"}}>
+                    <div style={{padding: 12, cursor:"pointer", display:"flex"}}
+                        onClick={()=> toggleDone(todo.id)}>
                       <FiCheck color="#CFFF48" size={26}/>
                     </div>
                   </div>}
