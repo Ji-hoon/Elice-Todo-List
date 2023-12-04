707f8a3c6a71aef15a57c8b3b8e7190fd192e0fc
[SW7] 김지훈 <lanslot84@gmail.com>
2023-12-04 14:50:01 +0900

    CHORE : express, webpack 설치 및 hydrate 로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import ReactDOM from 'react-dom/client' // 리액트돔 사용 (클라용)
+import { hydrateRoot } from 'react-dom/client'; // hydrate 사용 (서버용))
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+hydrateRoot(document.getElementById('root')!, 
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
+
